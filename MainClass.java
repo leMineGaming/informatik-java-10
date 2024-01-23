ac833f61d10c6dfba310680738c298443d4a03b7
XGamixX <GamingX@posteo.de>
2024-01-23 13:00:16 +0100

    Change 3 am 23.01.24

--- MainClass.java
+++ MainClass.java
@@ -1,6 +1,7 @@
 public class MainClass {
     public static void main(String[] args){
-        //Rennschnecke schnecke = new Rennschnecke();
+        Rennschnecke schnecke = new Rennschnecke("Bert", "Nacktschnecke", 10);
+        System.out.println(schnecke.name);
 
     }
 }
--- Rennschnecke.java
+++ Rennschnecke.java
@@ -3,6 +3,15 @@ class Rennschnecke{
     String rasse;
     int vMax;
     double strecke;
+    boolean xy;
+
+    Rennschnecke(String name, String rasse, int vMax){
+        this.name = name;
+        this.rasse = rasse;
+        this.vMax = vMax;
+        strecke = 0;
+        xy = false;
+    }
 
     void kriechen()
     {
