44114c9a4af9ae411229d0566a33b045a6881ac4
XGamixX <GamingX@posteo.de>
2024-01-25 08:37:20 +0100

    Changes am 25.01.24

--- Rennschnecke/MainClass.java
+++ Rennschnecke/MainClass.java
@@ -2,7 +2,8 @@ package Rennschnecke;
 public class MainClass {
     public static void main(String[] args){
         Rennschnecke schnecke = new Rennschnecke("Bert", "Nacktschnecke", 10);
-        System.out.println(schnecke.name);
-
+        System.out.println("Name = " + schnecke.name);
+        System.out.println("Rasse = " + schnecke.rasse);
+        System.out.println("vMax = " + schnecke.vMax);
     }
 }
