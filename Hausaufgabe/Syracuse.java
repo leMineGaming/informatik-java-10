f4f6acca0d7c35c9fce326658cfdc1b51e13ea92
XGamixX <>
2024-02-22 08:10:24 +0100

    Hausi

--- Hausaufgabe/Syracuse.java
+++ Hausaufgabe/Syracuse.java
@@ -1,1 +1,21 @@
 package Hausaufgabe;
+
+public class Syracuse {
+    public static void main(String[] args){
+        System.out.println("Gib eine natürliche Zahl ein!");
+        String eingegebenezahl = System.console().readLine();
+        int Number = Integer.parseInt(eingegebenezahl);
+        System.out.println("Gewählt: " + Number);
+        while (Number > 1){
+            if(Number % 2 == 0){
+                Number = Number /2;
+                System.out.println("Gerade: " + Number);
+            }
+            else{
+                Number = (3 * Number) + 1;
+                System.out.println("Ungerade: " + Number);
+            }
+        }
+
+        System.out.println("Ergebnis: " + Number);
+    }}
